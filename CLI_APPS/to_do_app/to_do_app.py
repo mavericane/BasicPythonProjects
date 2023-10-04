407d8affc3e02e0a5fd994aabe6ff3626259f44a
Amin <aminkhatoonabadi@gmail.com>
2023-10-04 17:31:06 +0330

    Updated CLI_APPS/to_do_app/to_do_app.py
    Version 2
    
    Create, view task

--- CLI_APPS/to_do_app/to_do_app.py
+++ CLI_APPS/to_do_app/to_do_app.py
@@ -2,7 +2,7 @@
 # Github Link: https://github.com/mavericane/
 # Website Link: https://mavericane.ir
 # Description: This is a simple to_do app. In this app you can simply create, edit, view, delete, mark a task az completed.
-# Version 1: Create a task
+# Version 2: Create, view a task
 # Importing required modules
 # platform module for detecting os
 import platform
@@ -15,6 +15,7 @@ import csv
 
 # termcolor module for colorizing outputs
 import termcolor
+
 
 # Detecting os and running file location
 def os_detect():
@@ -41,13 +42,15 @@ def os_detect():
         print("This program doesn't support your operating system")
         exit()
 
+
 # Checking if the tasks.csv file exists or doesn't exist to create a new tasks.csv file
 def tasks_csv_exists():
     if os.path.exists(file_location + "tasks.csv"):
         return True
     else:
         return False
-    
+
+
 # Creating a new tasks.csv file for tasks management
 def tasks_csv_create():
     data = ["id", "name", "status"]
@@ -56,12 +59,19 @@ def tasks_csv_create():
         csv_writer.writerow(data)
         csv_file.close()
 
+
 # Displaying main menu
 def display_menu():
     print(termcolor.colored("Options:", "cyan"))
-    print("1. Display tasks")
+    print("1. Display all tasks")
     print("2. Add task")
-    print("3. Quit")
+    print("3. Edit task")
+    print("4. View specific task")
+    print("5. Mark task as completed")
+    print("6. Delete specific task")
+    print("7. Delete all tasks(reset data)")
+    print("8. Quit")
+
 
 # Function to display the current tasks
 def display_tasks():
@@ -72,7 +82,7 @@ def display_tasks():
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             tasks.append(row)
-    
+
     # Removing the title's
     tasks.pop(0)
 
@@ -83,21 +93,18 @@ def display_tasks():
     uncompleted_tasks = []
     for task in tasks:
         if task[3].casefold() == "completed":
-             completed_tasks.append(task)
+            completed_tasks.append(task)
         elif task[3].casefold() == "uncompleted":
-             uncompleted_tasks.append(task)
+            uncompleted_tasks.append(task)
     tasks = completed_tasks + uncompleted_tasks
     completed_tasks_num = len(completed_tasks)
     uncompleted_tasks_num = len(uncompleted_tasks)
 
-    
     # Output to show all saved tasks
     if len(tasks) == 0:
-        print(
-            termcolor.colored("No tasks have been saved!", "yellow", "on_black")
-        )
+        print(termcolor.colored("No tasks have been saved!", "yellow", "on_black"))
         return None
-    
+
     print(
         termcolor.colored(
             f"There is total {total_tasks_num} tasks saved:",
@@ -113,8 +120,11 @@ def display_tasks():
             output_color = "green"
         elif task[3].casefold() == "uncompleted":
             output_color = "yellow"
-        print(f"Task ID: {termcolor.colored(task[0], output_color)} - Task name: {termcolor.colored(task[1], output_color)} - Status: {termcolor.colored(task[3], output_color)}")
+        print(
+            f"Task ID: {termcolor.colored(task[0], output_color)} - Task name: {termcolor.colored(task[1], output_color)} - Status: {termcolor.colored(task[3], output_color)}"
+        )
     print(termcolor.colored("`" * 3, "cyan", "on_black"))
+
 
 # Function to add a new task
 def add_task():
@@ -130,11 +140,15 @@ def add_task():
 
     # Removing the name title
     old_task_names.pop(0)
-    
+
     task_name = input("Please enter your task name: ")
 
     if task_name.casefold() in old_task_names:
-        print(termcolor.colored(f"You saved a task with {task_name} name before!", "yellow", "on_black"))
+        print(
+            termcolor.colored(
+                f"You saved a task with {task_name} name before!", "yellow", "on_black"
+            )
+        )
         while True:
             user_input = input("Do you want to edit your old task? (Y yes, N no): ")
             if user_input.casefold() == "y" or user_input.casefold() == "yes":
@@ -152,11 +166,17 @@ def add_task():
                 )
                 continue
             return None
-    print(termcolor.colored("Task Description is optional you can press Enter to skip it", "yellow", "on_black"))
+    print(
+        termcolor.colored(
+            "Task Description is optional you can press Enter to skip it",
+            "yellow",
+            "on_black",
+        )
+    )
     task_description = input("Please enter your task description(optional): ")
     if task_description.isspace():
-         task_description = ""
-    
+        task_description = ""
+
     # Data proccessing
     task = [id, task_name, task_description, "uncompleted"]
 
@@ -175,23 +195,136 @@ def add_task():
     )
 
 
-    
+# Function to edit a uncompleted task
+def edit_task(data=[]):
+    data = view_specific_task(data)
+
+    if data == None:
+        return None
+
+
+# Function to view a specific task
+def view_specific_task(data=[]):
+    if len(data) == 0:
+        while True:
+            while True:
+                print(
+                    termcolor.colored(
+                        "You can skip entering task name if you forget it!",
+                        "yellow",
+                        "on_black",
+                    )
+                )
+                task_name = input("Please enter the task name: ")
+                if task_name != "":
+                    skip_task_id = True
+                    empty_inputs = False
+                else:
+                    skip_task_id = False
+                break
+            if not skip_task_id:
+                while True:
+                    task_id = input("Please enter the task ID: ")
+                    if task_id != "":
+                        empty_inputs = False
+                    else:
+                        empty_inputs = True
+                    break
+            if empty_inputs:
+                print(
+                    termcolor.colored(
+                        "You must either enter task name or task ID!", "red", "on_black"
+                    )
+                )
+                while True:
+                    user_input = input("Do you want to try again? (Y yes, N no): ")
+                    if user_input.casefold() == "y" or user_input.casefold() == "yes":
+                        edit_task(data)
+                    elif user_input.casefold() == "n" or user_input.casefold() == "no":
+                        pass
+                    else:
+                        print(
+                            termcolor.colored(
+                                "Invalid choice. Please enter a valid option.",
+                                "red",
+                                "on_black",
+                            )
+                        )
+                        continue
+                    return None
+            break
+
+    # Searching task_name or task_ID in saved tasks
+    with open(file_location + "tasks.csv", "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if skip_task_id:
+                if task_name.casefold() == row[1].casefold():
+                    data = row
+                    break
+            else:
+                if task_id == row[0]:
+                    data = row
+                    break
+        csv_file.close()
+
+    # Checking if task_name or task_ID exists in saved tasks
+    if len(data) == 0:
+        print(
+            termcolor.colored(
+                "There is no saved task with such name or ID!",
+                "red",
+                "on_black",
+            )
+        )
+        return None
+
+    # Task details output
+    print(termcolor.colored("Task Detials: ", "green", "on_black"))
+    print(termcolor.colored("`" * 3, "cyan", "on_black"))
+    if data[3] == "completed":
+        output_color = "green"
+    elif data[3] == "uncompleted":
+        output_color = "yellow"
+    task_id = termcolor.colored(data[0], output_color)
+    print(f"Task ID: {task_id}")
+    task_name = termcolor.colored(data[1], output_color)
+    print(f"Task Name: {task_name}")
+    if data[2] != "":
+        task_description = termcolor.colored(data[2], output_color)
+        print(f"Task Description: {task_description}")
+    status = termcolor.colored(data[3].capitalize(), output_color)
+    print(f"Task status: {status}")
+    print(termcolor.colored("`" * 3, "cyan", "on_black"))
+
+    # Returning collected data to be used in other functions
+    return data
+
+
 # Function to mark a task as completed
-def complete_task():
-    #TODO
+def mark_task_completed():
+    # TODO
     pass
 
-# Function to delete a task
-def delete_task():
-    #TODO
+
+# Function to delete a specific task
+def delete_specific_task():
+    # TODO
     pass
+
+
+# Function to delete all tasks(reset data)
+def delete_all_tasks():
+    # TODO
+    pass
+
 
 if __name__ == "__main__":
     file_location = os_detect()
 
     if not tasks_csv_exists():
         tasks_csv_create()
-    
+
     while True:
         display_menu()
         choice = input("Enter your choice: ")
@@ -202,6 +335,16 @@ if __name__ == "__main__":
         elif choice == "2":
             add_task()
         elif choice == "3":
+            edit_task()
+        elif choice == "4":
+            view_specific_task()
+        elif choice == "5":
+            mark_task_completed()
+        elif choice == "6":
+            delete_specific_task()
+        elif choice == "7":
+            delete_all_tasks()
+        elif choice == "8":
             print("Goodbye!")
             break
         else:
